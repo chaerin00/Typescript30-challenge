d5feea1e6b29ad5fd4b7c94eb2823cce05fba933
chaerin <achrvv@sookmyung.ac.kr>
2022-04-06 19:39:36 +0900

    feat: implement upload file logic

--- myPictureUploader/script.js
+++ myPictureUploader/script.js
@@ -1,12 +1,19 @@
 const inputs = document.querySelectorAll(".controls input");
+const fileInput = document.querySelector(".album input");
 const image = document.querySelector("img");
 const handleUpdate = (e) => {
     const { dataset, name, value } = e.currentTarget;
     const suffix = dataset.sizing || "";
     document.documentElement.style.setProperty(`--${name}`, value + suffix);
 };
+const uploadImage = (e) => {
+    const target = e.target;
+    console.log(target.files);
+    const url = URL.createObjectURL(target.files[0]);
+    console.log(url);
+};
 inputs.forEach(input => {
     input.addEventListener("change", handleUpdate);
     input.addEventListener("mousemove", handleUpdate);
 });
-console.log(image.src);
+fileInput.addEventListener("change", uploadImage);
--- myPictureUploader/script.ts
+++ myPictureUploader/script.ts
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll(".controls input");
+const fileInput = document.querySelector(".album input");
 const image = document.querySelector("img");
 
 const handleUpdate = (e: MouseEvent) => {
@@ -7,9 +8,16 @@ const handleUpdate = (e: MouseEvent) => {
   document.documentElement.style.setProperty(`--${name}`, value + suffix);
 };
 
+const uploadImage = (e: MouseEvent) => {
+  const target = e.target as HTMLInputElement;
+  console.log(target.files);
+  const url = URL.createObjectURL(target.files[0]);
+  console.log(url);
+};
+
 inputs.forEach(input => {
   input.addEventListener("change", handleUpdate);
   input.addEventListener("mousemove", handleUpdate);
 });
 
-console.log(image.src);
+fileInput.addEventListener("change", uploadImage);
